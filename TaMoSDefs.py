0b02548aadf26a766ee350172baed75e8a902d1e
Tage Rahlen <tager@kth.se>
2020-10-16 17:17:45 +0200

    upload

---
+++ TaMoSDefs.py
@@ -0,0 +1,23 @@
+from random import randrange
+
+def main():
+    txt = open("defs.txt").read().split('\n')
+    txt = txt[:-1]
+    lengthtxt = len(txt)
+    name = []
+    defi = []
+    for item in txt:
+        temp = item.split(':')
+        name.append(temp[0])
+        defi.append(temp[1])
+
+    for item in range(0,20):
+        rand = randrange(lengthtxt)
+
+        print(name[rand])
+        input()
+        print(defi[rand])
+        input( '\n')
+
+if __name__ == "__main__":
+    main()
