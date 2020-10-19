9da4f94872ac2e4e2a6b33644cea40e0354a71a9
Tage Rahlen <tager@kth.se>
2020-10-19 11:51:24 +0200

    loopingAllDefs

--- TaMoSDefs.py
+++ TaMoSDefs.py
@@ -3,7 +3,6 @@ from random import randrange
 def main():
     txt = open("defs.txt").read().split('\n')
     txt = txt[:-1]
-    lengthtxt = len(txt)
     name = []
     defi = []
     for item in txt:
@@ -11,13 +10,14 @@ def main():
         name.append(temp[0])
         defi.append(temp[1])
 
-    for item in range(0,21):
+    for item in range(0,len(txt)):
+        lengthtxt = len(txt)
         rand = randrange(lengthtxt)
-
         print(name[rand])
         input()
         print(defi[rand])
         input( '\n')
+        txt.pop(rand)
 
 if __name__ == "__main__":
     main()
